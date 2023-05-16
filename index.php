f0c216a3e3c879389f2d30fcfa19944c86d4ff04
FrancescaLoria <francesca.loria90@hotmail.it>
2023-05-16 15:24:58 +0200

    costruttore

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
-//  - è definita una classe ‘Movie’
-   => all'interno della classe sono dichiarate delle variabili d'istanza
-   => all'interno della classe è definito un costruttore
-   => all'interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
 <?php
 
 class Movie {
     public $title;
     public $year;
-    public $director
+    public $director;
+
+    function __construct($_title) {
+        $this->title = $_title;
+    }
 
 }
+
+$newMovie = new Movie("La città incantata");
+
+echo $newMovie->title;
 
 ?>
