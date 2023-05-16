f52f8cb836afb7cbe69502ed1a741da23ead4ca3
FrancescaLoria <francesca.loria90@hotmail.it>
2023-05-16 15:46:10 +0200

    metodo

--- index.php
+++ index.php
@@ -6,14 +6,27 @@ class Movie {
     public $year;
     public $director;
 
-    function __construct($_title) {
+    function __construct($_title,$_director,$_year) {
         $this->title = $_title;
+        $this->director = $_director;
+        $this->year = $_year;
+    }
+
+    public function generateMovieDetails() {
+        return "Il film " . $this->title . " ha come regista " . $this->director . ", ed è stato realizzato nel " . $this->year;
+               
+
     }
 
 }
 
-$newMovie = new Movie("La città incantata");
+$newMovie = new Movie("La città incantata","Hayao Miyazaki", 2001);
 
-echo $newMovie->title;
+//echo "Titolo del film: " . $newMovie->title;
+
+$filmDetails = $newMovie->generateMovieDetails();
+
+echo $filmDetails;
+
 
 ?>
